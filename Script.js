ab9e31898cc7f6e17ae6b99cdf1bae09a85b11d6
Pouya Babaie <pouyababaie123@gmail.com>
2021-03-15 11:56:49 +0330

    The Changes has been made

---
+++ Script.js
@@ -0,0 +1,12 @@
+import Home from "./main"
+
+const Home1 = new Home(
+    "Russian Steel",
+    "Russian Wood",
+    ["Dark", 3],
+    15,
+    18,
+    12,
+    "Iranian Steel"
+);
+console.log("this is the material of the secend Door: ", Home1.SecendDoor);
--- main.js
+++ main.js
@@ -1,13 +1,34 @@
-const Home={
-doorOpen : false,
-address : "Karaj",
-TVon: true,
-ferniture : ["TV","Bokhari","Doors",""],
-Kitchen:{
-    Dooropen : true,
-    KitchenFanOn : false,
-     
-}
-
-
+class Home {
+    constructor(
+        mainDoor,
+        SecendDoor,
+        mainHall,
+        Bedroom1,
+        Bedroom2,
+        kitchen,
+        backDoor
+    )
+    {
+        this.mainDoor = mainDoor;
+        this.SecendDoor = SecendDoor;
+        this.mainHall = mainHall;
+        this.Bedroom1 = Bedroom1;
+        this.Bedroom2 = Bedroom2;
+        this.kitchen = kitchen;
+        this.backDoor = backDoor;
+        
+    }
+    mainDoorOpen(ToggleDoormain){
+        this.mainDoor = ToggleDoormain;
+    }
+    SecendDoorOpen(ToggleDoorSecend){
+        this.SecendDoor = ToggleDoorSecend;
+    }
+    KitchenDoorOpen(ToggleDoorKitcen){
+        this.kitchen = ToggleDoorKitcen;
+    }
+    BackDoorOpen(ToggleDoorback){
+        this.backDoor = ToggleDoorback;
+    }
 };
+export default Home
