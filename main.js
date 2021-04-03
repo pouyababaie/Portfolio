c0b08153c210bf8233b66d7cd7f794b2f8da9744
Pouya Babaie <pouyababaie123@gmail.com>
2021-04-03 14:32:56 +0430

    a new page landed and other files modified

--- main.js
+++ main.js
@@ -1,34 +1,13 @@
-class Home {
-    constructor(
-        mainDoor,
-        SecendDoor,
-        mainHall,
-        Bedroom1,
-        Bedroom2,
-        kitchen,
-        backDoor
-    )
-    {
-        this.mainDoor = mainDoor;
-        this.SecendDoor = SecendDoor;
-        this.mainHall = mainHall;
-        this.Bedroom1 = Bedroom1;
-        this.Bedroom2 = Bedroom2;
-        this.kitchen = kitchen;
-        this.backDoor = backDoor;
-        
-    }
-    mainDoorOpen(ToggleDoormain){
-        this.mainDoor = ToggleDoormain;
-    }
-    SecendDoorOpen(ToggleDoorSecend){
-        this.SecendDoor = ToggleDoorSecend;
-    }
-    KitchenDoorOpen(ToggleDoorKitcen){
-        this.kitchen = ToggleDoorKitcen;
-    }
-    BackDoorOpen(ToggleDoorback){
-        this.backDoor = ToggleDoorback;
-    }
+const Home={
+doorOpen : false,
+address : "Karaj",
+TVon: true,
+ferniture : ["TV","Bokhari","Doors",""],
+Kitchen:{
+    Dooropen : true,
+    KitchenFanOn : false,
+     
+}
+
+
 };
-export default Home
